9aaef3e48be8647eb7ecde69fc4ae169666bf065
mjaquiery <m.jaquiery@sussex.ac.uk>
2018-06-25 22:27:06 +0100

    slurm

--- runARC.sh
+++ runARC.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# set the number of nodes
 #SBATCH --nodes=1
+
+# set max wallclock time
 #SBATCH --time=01:00:00
+
+# set name of job
 #SBATCH --job-name=EvoEgoBias
+
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+
+# send mail to this address
+#SBATCH --mail-user=matt.jaquiery@psy.ox.ac.uk
 
 module purge
 module load R
