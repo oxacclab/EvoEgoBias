561cc1931eeeb99ad0b4399175526a1f4db918ea
mjaquiery <m.jaquiery@sussex.ac.uk>
2018-06-27 11:53:46 +0100

    ARC test

--- model1.R
+++ model1.R
@@ -32,110 +32,113 @@ if(ARC) {
 
 # Parameter space to explore
 degrees <- c(2)
-sensitivitySDs <- c(1)
+sensitivitySDs <- c(0, 1, 5, 10)
 
 
 # Define the function
-  func <- function(x) {
-    source('evoSim/evoSim/R/evoSim.R')
-    data <- evoSim(agentCount = 150,
-                   agentDegree = 100,
-                   decisionCount = 150,
-                   generationCount = 1500,
-                   mutationChance = 0.01,
-                   other = list(sensitivitySD = sensitivitySD),
-                   makeAgentFun = function(modelParams, parents = NULL) {
-                     # Inherit egoBias if there's a previous generation and we're not mutating
-                     if(!is.null(parents)) {
-                       # print(paste0('id: ', parents$id,
-                       #              '; egoBias: ', round(parents$egoBias,3),
-                       #              '; fitness: ', round(parents$fitness,3)))
-                       if(runif(1) < modelParams$mutationChance) {
-                         # mutate
-                         egoBias <- rnorm(1, parents$egoBias, 0.1)
-                       } else {
-                         egoBias <- parents$egoBias
-                       }
-                     }
-                     else {
-                       # print(paste('novelty',parents$generation))
-                       egoBias <- rnorm(1, .5, 1)
-                     }
-                     sensitivity <- abs(rnorm(1, mean = 10, sd = 5))
-                     # Keep egoBias to within [0-1]
-                     egoBias <- clamp(egoBias, maxVal = 1, minVal = 0)
-                     return(data.frame(sensitivity, egoBias))
-                   },
-                   selectParentsFun = function(modelParams, agents, world, ties) {
-                     tmp <- agents[which(agents$generation == world$generation),]
-                     tmp <- tmp[order(tmp$fitness, decreasing = T),]
-                     # drop the worst half of the population
-                     # tmp <- tmp[1:2,]#(floor(nrow(tmp)/2)), ]
-                     # the others get weighted by relative fitness which are transformed to +ve values
-                     tmp$fitness <- tmp$fitness - min(tmp$fitness) + 1
-                     # scale appropriately
-                     while(any(tmp$fitness < 10))
-                       tmp$fitness <- tmp$fitness * 10
-                     # and round off
-                     tmp$fitness <- round(tmp$fitness)
-                     tickets <- vector(length = sum(tmp$fitness)) # each success buys a ticket in the draw
-                     i <- 0
-                     for(a in 1:nrow(tmp)) {
-                       tickets[(i+1):(i+1+tmp$fitness[a])] <- a
-                       i <- i + 1 + tmp$fitness[a]
-                     }
-                     winners <- sample(tickets, modelParams$agentCount, replace = T)
-                     # The winners clone their egocentric discounting
-                     winners <- tmp[winners,'id']
-                     #print(cor(agents$egoBias, abs(agents$fitness-.5)))
-                     # print(summary(agents[agents$id %in% winners, c('egoBias', 'fitness')]))
-                     # print(tmp[tmp$id %in% winners,])
-                     return(winners)
-                   },
-                   getDecisionFun = function(modelParams, agents, world, ties, initial = F) {
-                     mask <- which(agents$generation == world$generation)
-                     if(initial) {
-                       # initial decision - look and see
-                       n <- length(mask)
-                       agents$initialDecision[mask] <- rnorm(n, 
-                                                             rep(world$state, n), 
-                                                             clamp(agents$sensitivity[mask],Inf))
+func <- function(x) {
+  source('evoSim/evoSim/R/evoSim.R')
+  data <- evoSim(agentCount = 150,
+                 agentDegree = 100,
+                 decisionCount = 150,
+                 generationCount = 1500,
+                 mutationChance = 0.01,
+                 other = list(sensitivitySD = sensitivitySD),
+                 makeAgentFun = function(modelParams, parents = NULL) {
+                   # Inherit egoBias if there's a previous generation and we're not mutating
+                   if(!is.null(parents)) {
+                     # print(paste0('id: ', parents$id,
+                     #              '; egoBias: ', round(parents$egoBias,3),
+                     #              '; fitness: ', round(parents$fitness,3)))
+                     if(runif(1) < modelParams$mutationChance) {
+                       # mutate
+                       egoBias <- rnorm(1, parents$egoBias, 0.1)
                      } else {
-                       # Final decision - take advice
-                       # Use vector math to do the advice taking
-                       out <- NULL
-                       out <- (agents$initialDecision[mask] * agents$egoBias[mask]) + 
-                         ((1-agents$egoBias[mask]) * agents$advice[mask])
-                       out[is.na(out)] <- agents$initialDecision[mask][is.na(out)]
-                       agents$finalDecision[mask] <- out
+                       egoBias <- parents$egoBias
                      }
-                     return(agents)
-                   },
-                   getWorldStateFun = function(modelParams, world) {
-                     return(50)
-                   })
-    # save results
-    n <- length(unique(data$agents$generation))
-    results <- data.frame(repetition = rep(x, n),
-                          # degree = rep(degree, n),
-                          sensitivitySD = rep(sensitivitySD, n),
-                          generation = unique(data$agents$generation), 
-                          modelDuration = rep(data$duration, n))
-    # bind in the stats of interest aggregated by the generation
-    results <- cbind(results, 
-                     aggregate(data$agents, 
-                               list(data$agents$generation), 
-                               mean)[ ,c('fitness', 
-                                         'degree', 
-                                         'sensitivity', 
-                                         'egoBias', 
-                                         'initialDecision', 
-                                         'finalDecision')])
-    rawdata <- data
-    rawdata$rep <- x
-    return(list(rawdata = rawdata, results = results))
-  }
+                   }
+                   else {
+                     # print(paste('novelty',parents$generation))
+                     egoBias <- rnorm(1, .5, 1)
+                   }
+                   sensitivity <- abs(rnorm(1, mean = 10, sd = 5))
+                   # Keep egoBias to within [0-1]
+                   egoBias <- clamp(egoBias, maxVal = 1, minVal = 0)
+                   return(data.frame(sensitivity, egoBias))
+                 },
+                 selectParentsFun = function(modelParams, agents, world, ties) {
+                   tmp <- agents[which(agents$generation == world$generation),]
+                   tmp <- tmp[order(tmp$fitness, decreasing = T),]
+                   # drop the worst half of the population
+                   # tmp <- tmp[1:2,]#(floor(nrow(tmp)/2)), ]
+                   # the others get weighted by relative fitness which are transformed to +ve values
+                   tmp$fitness <- tmp$fitness - min(tmp$fitness) + 1
+                   # scale appropriately
+                   while(any(tmp$fitness < 10))
+                     tmp$fitness <- tmp$fitness * 10
+                   # and round off
+                   tmp$fitness <- round(tmp$fitness)
+                   tickets <- vector(length = sum(tmp$fitness)) # each success buys a ticket in the draw
+                   i <- 0
+                   for(a in 1:nrow(tmp)) {
+                     tickets[(i+1):(i+1+tmp$fitness[a])] <- a
+                     i <- i + 1 + tmp$fitness[a]
+                   }
+                   winners <- sample(tickets, modelParams$agentCount, replace = T)
+                   # The winners clone their egocentric discounting
+                   winners <- tmp[winners,'id']
+                   #print(cor(agents$egoBias, abs(agents$fitness-.5)))
+                   # print(summary(agents[agents$id %in% winners, c('egoBias', 'fitness')]))
+                   # print(tmp[tmp$id %in% winners,])
+                   return(winners)
+                 },
+                 getDecisionFun = function(modelParams, agents, world, ties, initial = F) {
+                   mask <- which(agents$generation == world$generation)
+                   if(initial) {
+                     # initial decision - look and see
+                     n <- length(mask)
+                     agents$initialDecision[mask] <- rnorm(n, 
+                                                           rep(world$state, n), 
+                                                           clamp(agents$sensitivity[mask],Inf))
+                   } else {
+                     # Final decision - take advice
+                     # Use vector math to do the advice taking
+                     out <- NULL
+                     out <- (agents$initialDecision[mask] * agents$egoBias[mask]) + 
+                       ((1-agents$egoBias[mask]) * agents$advice[mask])
+                     out[is.na(out)] <- agents$initialDecision[mask][is.na(out)]
+                     agents$finalDecision[mask] <- out
+                   }
+                   return(agents)
+                 },
+                 getWorldStateFun = function(modelParams, world) {
+                   return(50)
+                 })
+  # save results
+  n <- length(unique(data$agents$generation))
+  results <- data.frame(repetition = rep(x, n),
+                        # degree = rep(degree, n),
+                        sensitivitySD = rep(sensitivitySD, n),
+                        generation = unique(data$agents$generation), 
+                        modelDuration = rep(data$duration, n))
+  # bind in the stats of interest aggregated by the generation
+  results <- cbind(results, 
+                   aggregate(data$agents, 
+                             list(data$agents$generation), 
+                             mean)[ ,c('fitness', 
+                                       'degree', 
+                                       'sensitivity', 
+                                       'egoBias', 
+                                       'initialDecision', 
+                                       'finalDecision')])
+  rawdata <- data
+  rawdata$rep <- x
+  return(list(rawdata = rawdata, results = results))
+}
 
+func <- function(x) {
+  return(list(rawdata = paste('hi from',x), results = data.frame(sample(1:10,10))))
+}
   
 # Run the models
 for(sensitivitySD in sensitivitySDs) {
